6c6c731331659829b7e5f2a6a9119057e4e14b1b
Dinifbryn <92310731+Dinifbryn@users.noreply.github.com>
2021-12-29 21:45:21 +0700

    Add files via upload

---
+++ otentikasi.js
@@ -0,0 +1,11 @@
+function startlog(){
+    var inp = document.getElementById("username").value 
+    + document.getElementById("password").value;
+        var inp2 = ("dinifbryn62") + ("dinifbb256");
+        if (inp == inp2){
+    document.getElementById('akses') + alert("Login Berhasil") + document.write("Login Berhasil");
+        }
+        else if (inp != inp2){
+            document.getElementById('akses') + alert("Login Belum Berhasil")
+        }
+}
