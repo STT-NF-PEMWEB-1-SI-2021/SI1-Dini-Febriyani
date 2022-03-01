b0f48b1c02ce842d8957df25e22a1e935b9d046c
Dinifbryn <92310731+Dinifbryn@users.noreply.github.com>
2022-03-01 21:39:39 +0700

    Add files via upload

---
+++ PRAKTIKUM-1-PHP/array_siswa.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Daftar Siswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        .table{
+            margin-right: 1em;
+            margin-top: 2em;
+            background-color: #a1d8f5;
+        }
+        h3{
+            margin-top: 12px;
+            text-align: center;
+        }
+    </style>
+  </head>
+  <body>
+    <h3>Daftar Nilai Siswa</h3>
+    <?php 
+        $ns1 = ['id'=>1,'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78];
+        $ns2 = ['id'=>2,'nim'=>'01121','uts'=>70,'uas'=>50,'tugas'=>68];
+        $ns3 = ['id'=>3,'nim'=>'01130','uts'=>60,'uas'=>86,'tugas'=>70];
+        $ns4 = ['id'=>4,'nim'=>'01134','uts'=>90,'uas'=>91,'tugas'=>82];
+
+        $ar_nilai = [$ns1, $ns2 , $ns3, $ns4];
+
+    ?>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">NIM</th>
+      <th scope="col">UTS</th>
+      <th scope="col">UAS</th>
+      <th scope="col">Tugas</th>
+      <th scope="col">Nilai AKhir</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+    $nomor = 1; 
+    foreach($ar_nilai as $ns){
+        echo '<tr><td>'.$nomor.'</td>';
+        echo '<td>'.$ns['nim'].'</td>';
+        echo '<td>'.$ns['uts'].'</td>';
+        echo '<td>'.$ns['uas'].'</td>';
+        echo '<td>'.$ns['tugas'].'</td>';
+        $nilai_akhir = ($ns['uts'] + $ns['uas']+$ns['tugas'])/3;
+        echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>';
+        echo '<tr/>';
+        $nomor++;
+    }
+   ?>
+  </tbody>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+
---
+++ PRAKTIKUM-1-PHP/variable_user.php
@@ -0,0 +1,14 @@
+<?php
+    // definisikan variables
+
+    $nama = 'Huang Renjun';
+    $umur = 22;
+    $berat = 60.5;
+
+    echo 'Nama :' . $nama;
+    echo '<br/>Umur :' . $umur. 'Tahun';
+    echo '<br/>Berat :' . $berat. 'KG';
+
+    echo '<br/>Hello '. $nama. ' Apakabar?';
+
+?>
