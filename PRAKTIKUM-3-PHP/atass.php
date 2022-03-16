ecedcadd83205b18592e3a814345b42d212e51fe
Dinifbryn <92310731+Dinifbryn@users.noreply.github.com>
2022-03-16 22:14:45 +0700

    Add files via upload

---
+++ PRAKTIKUM-3-PHP/atass.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SAS-STTNF</title>
+</head>
+<body>
+    <header role="banner">
+        <h3>Student Activity Score - STTNF</h3><hr>
+    </header>
---
+++ PRAKTIKUM-3-PHP/bawah.php
@@ -0,0 +1,5 @@
+    <footer>
+        <p>Created by Pusinfo NF &copy;2017</a></p>
+    </footer>   
+</body>
+</html>
